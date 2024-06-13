9ae0c93e5ce1c512f0825466c6f0631127452fd2
Boris Ruf <boris.ruf@axa.com>
2024-06-13 14:39:53 +0200

    initial commit

---
+++ mockai/mockai.py
@@ -0,0 +1,35 @@
+import random
+import time
+import difflib
+import pickle
+import warnings
+
+class MockAzureOpenAI:
+    class chat:
+        class completions:
+            def create(model, messages, temperature=1, max_tokens=16, top_p=1, frequency_penalty=0, presence_penalty=0, stop=None):
+
+                    with open('cache.pickle', 'rb') as f:
+                        cache = pickle.load(f)
+
+                    if model in cache.keys():
+   
+                        # Fuzzy match query to cached queries
+                        match = difflib.get_close_matches(messages[0]['content'], cache[model].keys(), n=1, cutoff=0.9)
+                        key = match[0] if match else 'default' # Use default responses if no match
+                        
+                        responses = cache[model].get(key)
+                
+                        # Randomly delay response to simulate API request
+                        time.sleep(random.uniform(1, 3))
+
+                        # Randomly select one of the matching repsonses
+                        return responses[random.randint(0, len(responses)-1)]
+
+                    else:
+                        warnings.warn(f"No cached data available for the AI model '{model}'")
+                        return None
+
+
+    def __init__(self, azure_endpoint, api_key, api_version=None):
+        pass
---
+++ mockai/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setup(
+    name='mockai',
+    packages=find_packages(include=['mockai']),
+    version='0.1.0',
+    description='Simulates the use of cloud-based API models.',
+    long_description='A Python library which simulates the use of cloud-based API models for educational purposes.',
+    author='Boris Ruf',
+    url='https://github.com/borisruf/mockai',
+    install_requires=[
+        'random',
+        'time',
+        'difflib',
+        'pickle',
+        'warnings'
+    ]
+)
