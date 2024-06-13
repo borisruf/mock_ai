7915adeb240b14883c561a80bf24c51ff1349632
Boris Ruf <boris.ruf@axa.com>
2024-06-13 16:44:01 +0200

    include pickle

--- mockai/mock_azure_openai.py
+++ mockai/mock_azure_openai.py
@@ -3,16 +3,13 @@ import time
 import difflib
 import pickle
 import warnings
-import os
 
 class MockAzureOpenAI:
     class chat:
         class completions:
             def create(model, messages, temperature=1, max_tokens=16, top_p=1, frequency_penalty=0, presence_penalty=0, stop=None):
-
-                    path = os.path.join(os.path.dirname(__file__), 'cache.pickle')
-
-                    with open(path, 'rb') as f:
+                    
+                    with open('cache.pickle', 'rb') as f:
                         cache = pickle.load(f)
 
                     if model in cache.keys():
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mockai',
     packages=find_packages(),
+    package_data={'mockai': ['cache.pickle']},
     version='0.1.0',
     description='Simulates the use of cloud-based API models.',
     long_description='A Python library which simulates the use of cloud-based API models for educational purposes.',
