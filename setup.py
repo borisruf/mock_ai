765113ce1390a2c93b65c180ed40b9ba09b0897e
Boris Ruf <boris.ruf@axa.com>
2024-06-13 15:07:11 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+setup(
     name='mockai',
     packages=find_packages(include=['mockai']),
     version='0.1.0',
