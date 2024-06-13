177f5b91d03c31723eb44b2dbad63aece0f62e01
Boris Ruf <boris.ruf@axa.com>
2024-06-13 16:10:07 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mockai',
-    packages=find_packages(include=['mockai']),
+    packages=find_packages(),
     version='0.1.0',
     description='Simulates the use of cloud-based API models.',
     long_description='A Python library which simulates the use of cloud-based API models for educational purposes.',
